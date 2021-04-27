Fixed size calculation

example/test_create_network.py
@@ -1,4 +1,3 @@
-from torch.nn.modules.conv import ConvTranspose2d
 from ptu.network_util import create_network, create_reverse_network
 from ptu.util.types import LayerType
 from torchinfo import summary
@@ -44,7 +43,9 @@ network_config_conv = [
 class ModelForward(nn.Module):
     def __init__(self):
         super().__init__()
-        self.net = create_network(network_config_conv, 3, 64)
+        self.net, curr_channels, size = create_network(network_config_conv, 3, 64)
+        print(curr_channels)
+        print(size)
 
     def forward(self, x):
         return self.net(x)
@@ -53,7 +54,9 @@ class ModelForward(nn.Module):
 class ModelReversed(nn.Module):
     def __init__(self):
         super().__init__()
-        self.net = create_reverse_network(network_config_conv, 3, 64)
+        self.net, curr_channels, size = create_reverse_network(network_config_conv, 3, 64)
+        print(curr_channels)
+        print(size)
 
     def forward(self, x):
         return self.net(x)

ptu/network_util.py
@@ -23,7 +23,7 @@ def create_network(config, in_channels, in_size):
     for layer_type, layer_attr in config:
         layer, curr_channels, size = _process_layer(layer_type, layer_attr, curr_channels, size)
         layers.append(layer)
-    return nn.Sequential(*layers)
+    return nn.Sequential(*layers), curr_channels, size
 
 
 # Create the network backwards in reference to the given config
@@ -41,7 +41,7 @@ def create_reverse_network(config, out_channels, out_size):
     for layer_type, layer_attr in config:
         layer, curr_channels, size = _process_layer_reversed(layer_type, layer_attr, curr_channels, size)
         layers.insert(0, layer)
-    return nn.Sequential(*layers)
+    return nn.Sequential(*layers), curr_channels, size
 
 
 def _process_layer(layer_type, layer_attr, in_channels, size):
@@ -108,7 +108,7 @@ def process_flat(layer_attr, curr_channels, curr_size):
 
 def process_flat_reversed(layer_attr, curr_channels, curr_size):
     size = int(np.sqrt(curr_size))
-    return nn.Unflatten(1, (curr_channels, size, size)), curr_channels, curr_size * size * size
+    return nn.Unflatten(1, (curr_channels, size, size)), curr_channels, curr_channels * size * size
 
 
 def process_linear(layer_attr, curr_channels, curr_size):